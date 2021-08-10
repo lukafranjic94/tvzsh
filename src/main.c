c166450896f99bb93f28c75a86b29c1d33176788
Luka Franjić <lukafranjic94@gmail.com>
2021-08-11 01:03:35 +0200

    Change main loop to a do while

--- src/main.c
+++ src/main.c
@@ -8,14 +8,12 @@ int main(void) {
   printf("Welcome\n");
   size_t bufsize = 256;
   char *buffer = malloc(bufsize * sizeof(char));
-  while (1) {
+  int code = 0;
+  do {
     print_sh_prefix();
     getline(&buffer, &bufsize, stdin);
-    int code = analyze_input(buffer);
-    if (code == 0) {
-      break;
-    }
-  }
+    code = analyze_input(buffer);
+  } while (code);
   free(buffer);
   printf("Exiting...\n");
   return 0;
